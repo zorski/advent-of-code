a89ef1fa13270da0faa7b5a5bad4d0e96b7a25cd
zorski <m.zaorski@icloud.com>
2019-01-29 00:29:10 +0100

    day 5  - part1 recursion fails haaaRD

--- ps/5day.ps1
+++ ps/5day.ps1
@@ -1,5 +1,6 @@
-$Polymer = "abAcCaCBAcCcaDA"
-
+$Polymer = Get-Content -Path .\inputs\5day.txt
+# $Polymer = ""
+# $Polymer = "dabAcCaCBAcCcaDA"
 function Test-Letter {
     # Function tests if letters are the same but different case:
     # a ? A -> true
@@ -14,17 +15,18 @@ function Test-Letter {
     [System.Math]::Abs([byte]$FirstChar - [byte]$SecondChar) -eq 32  
 }
 function React-Polymer {
+    [cmdletbinding()]
     param (
         [string]$Polymer  
     )
     
-    for ($i = 0; $i -lt ($Polymer.Length - 1); $i++) {
+    for ($i = (0 + [math]::Max(0,($i-1))); $i -lt ($Polymer.Length - 1); $i++) {
         if (Test-Letter -FirstChar $Polymer[$i] -SecondChar $Polymer[$i + 1]) {
             # remove from string these two letters
-            $Polymer = React-Polymer -Polymer $Polymer.Remove($i, 2)   
-            $Polymer
+            $Polymer = React-Polymer -Polymer $Polymer.Remove($i, 2)
         }
     }
+    return $Polymer
 }
 
 React-Polymer -Polymer $Polymer

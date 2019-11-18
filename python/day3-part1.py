0b2f439bde9988ede08b5aefbaefb784accb68b7
zorski <m.zaorski@icloud.com>
2019-11-18 23:59:28 +0100

    changed parse_coordinates function added stupid loop at the end

--- python/day3-part1.py
+++ python/day3-part1.py
@@ -5,16 +5,17 @@ input_path = os.path.join(os.getcwd(), 'input', 'day3-part1-testinput.txt')
 claims = [line.rstrip('\n') for line in open(input_path)]
 parsed_claims = []
 
+
 def parse_coordinates(coordinates: string):
-    coordinates_set = set()
+    coordinates_list = list()
     start_point = tuple(coordinates.split(':')[0].strip().split(','))
     c = tuple(coordinates.split(':')[1].strip().split('x'))
 
     for i in range(1, int(c[0]) + 1):
         for j in range(1, int(c[1]) + 1):
-            coordinates_set.add((int(start_point[0]) + i, int(start_point[1]) + j))
+            coordinates_list.append((int(start_point[0]) + i, int(start_point[1]) + j))
 
-    return coordinates_set
+    return coordinates_list
 
 
 def parse_claim(claim: string):
@@ -27,7 +28,21 @@ def parse_claim(claim: string):
 for claim in claims:
     parsed_claims.append(parse_claim(claim))
 
-print('echo')
+
+counter = 0
+coords = set()
+already_counted = set()
+
+for parsed_claim in parsed_claims:
+    for coord in parsed_claim['coordinates']:
+        if coord in coords and coord not in already_counted:
+            counter += 1
+            already_counted.add(coord)
+        else:
+            coords.add(coord)
+
+
+print("There are {} square inches of fabric within two or more claims".format(str(counter)))
 
 
 

9a0dd18b8ff0bea0405b0cb794c9c20ac305e607
zorski <R2d21987p10!>
2020-02-12 21:32:26 +0100

    day 6 part 1 done

--- python/day6.py
+++ python/day6.py
@@ -4,7 +4,7 @@ from pprint import pprint as pp
 
 input_path = os.path.join(os.getcwd(), 'input', 'day6-input.txt')
 raw_coordinates = [line.rstrip('\n') for line in open(input_path)]
-area = [[['X:X', sys.maxsize] for i in range(10)] for j in range(10)]
+area = [[['X:X', sys.maxsize] for i in range(400)] for j in range(400)]
 
 
 def parse_coordinates(coordinates):
@@ -20,10 +20,11 @@ def manhattan_distance(a: tuple, b: tuple) -> int:
 
 
 coordinates = parse_coordinates(raw_coordinates)
-max_manhattan_distance = sys.maxsize
 
-for i in range(10):
-    for j in range(10):
+# for each coordinate on the grid 400x400 (hardcoded :/) compare the distance from manhattan_distance function to
+# current value
+for i in range(400):
+    for j in range(400):
         for c in coordinates:
             distance = manhattan_distance(tuple([i, j]), c)
             if distance == area[i][j][-1]:
@@ -31,13 +32,39 @@ for i in range(10):
                 area[i][j][-1] = distance
 
             if distance < area[i][j][-1]:
-                element = [','.join([str(i) for i in c]), distance]
+                element = [', '.join([str(i) for i in c]), distance]
                 area[i][j] = element
 
+coordinates_for_exclusion = set()
+
+x_zero_axis = set([i[0] for i in area[0] if i[0] != '.'])
+x_axis = set([i[0] for i in area[-1] if i[0] != '.'])
+coordinates_for_exclusion.update(x_zero_axis)
+coordinates_for_exclusion.update(x_axis)
+
+for idx, val in enumerate(area):
+    first = val[0][0]
+    last = val[-1][0]
+    if first != '.':
+        coordinates_for_exclusion.add(first)
+    if last != '.':
+        coordinates_for_exclusion.add(last)
+
+meh_coordinates = set(raw_coordinates)
+desired_coordinates = meh_coordinates.difference(coordinates_for_exclusion)
+
+coord_counter = {}
+for dc in desired_coordinates:
+    coord_counter[dc] = 0
 
 
+for i in range(400):
+    for j in range(400):
+        for dc in desired_coordinates:
+            if area[i][j][0] == dc:
+                coord_counter[dc] += 1
 
-pp(area)
+print(max(coord_counter.values()))
 
 
 
